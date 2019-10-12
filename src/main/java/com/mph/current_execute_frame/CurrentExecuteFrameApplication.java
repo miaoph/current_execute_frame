7eca6cb2d039b80b1afbbb20ecf5d1ba4d41f90a
miaoph <miaoph@52iuh.com>
2019-10-12 15:58:35 +0800

    init

---
+++ src/main/java/com/mph/current_execute_frame/CurrentExecuteFrameApplication.java
@@ -0,0 +1,13 @@
+package com.mph.current_execute_frame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentExecuteFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrentExecuteFrameApplication.class, args);
+    }
+
+}
